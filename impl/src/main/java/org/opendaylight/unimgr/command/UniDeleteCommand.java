fbf9529cbf3a00ea48fc642180050ff75b59cffd
Mohamed El-Serngawy <melserngawy@inocybe.com>
2015-11-03 18:10:15 -0500

    WIP delete UNI command
    Change-Id: I0b531a84ab9e0ac8c7bdab9ebc0013e1039efe13
    Signed-off-by: Mohamed El-Serngawy <melserngawy@inocybe.com>

--- impl/src/main/java/org/opendaylight/unimgr/command/UniDeleteCommand.java
+++ impl/src/main/java/org/opendaylight/unimgr/command/UniDeleteCommand.java
@@ -9,15 +9,26 @@ package org.opendaylight.unimgr.command;
 
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ExecutionException;
 
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.ReadTransaction;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
 import org.opendaylight.unimgr.impl.UnimgrUtils;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.unimgr.rev151012.OvsdbNodeRef;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.unimgr.rev151012.Uni;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Optional;
+import com.google.common.util.concurrent.CheckedFuture;
 
 public class UniDeleteCommand extends AbstractDeleteCommand {
 
@@ -35,9 +46,37 @@ public class UniDeleteCommand extends AbstractDeleteCommand {
         Set<InstanceIdentifier<Uni>> removedUnis = UnimgrUtils.extractRemoved(changes, Uni.class);
         if (!removedUnis.isEmpty()) {
             for (InstanceIdentifier<Uni> removedUniIid: removedUnis) {
+                Optional<Uni> optUni = readUni(removedUniIid);
+                if(optUni.isPresent()){
+                    Uni uni = optUni.get();
+                    OvsdbNodeRef ovsNdRef = uni.getOvsdbNodeRef();
+                    InstanceIdentifier<Node> iidNode = (InstanceIdentifier<Node>) ovsNdRef.getValue();
+                    WriteTransaction transaction = dataBroker.newWriteOnlyTransaction();
+                    transaction.delete(LogicalDatastoreType.OPERATIONAL, iidNode);
+                    CheckedFuture<Void, TransactionCommitFailedException> future = transaction.submit();
+                    try {
+                        future.checkedGet();
+                    } catch (TransactionCommitFailedException e) {
+                        LOG.warn("Failed to delete {} ", iidNode, e);
+                    }
+                }
                 LOG.info("Received a request to remove an UNI ", removedUniIid);
             }
         }
     }
 
+    private final Optional<Uni> readUni(InstanceIdentifier<?> genericUNI) {
+        ReadTransaction read = dataBroker.newReadOnlyTransaction();
+        InstanceIdentifier<Uni> iidUni = genericUNI.firstIdentifierOf(Uni.class);
+        CheckedFuture<Optional<Uni>, ReadFailedException> uniFuture = read.read(LogicalDatastoreType.OPERATIONAL, iidUni);
+        Optional<Uni> uniOptional;
+        try {
+            uniOptional = uniFuture.get();
+            return uniOptional;
+        } catch (InterruptedException e) {
+            return Optional.absent();
+        } catch (ExecutionException e) {
+            return Optional.absent();
+        }
+    }
 }
