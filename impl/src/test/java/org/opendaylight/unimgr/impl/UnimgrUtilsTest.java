2cf6d12930e3450190f25067196d54654440e323
grmontpetit <grmontpetit@inocybe.com>
2016-01-16 17:59:32 -0500

    (WIP) Added scaffolding for tests and example functions
    Change-Id: I80272c402e82cfcce942502c4759a56b12bb07e9
    Signed-off-by: grmontpetit <grmontpetit@inocybe.com>

---
+++ impl/src/test/java/org/opendaylight/unimgr/impl/UnimgrUtilsTest.java
@@ -0,0 +1,197 @@
+package org.opendaylight.unimgr.impl;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UnimgrUtilsTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /*
+     *  This test for 2 functions with the
+     *  same name that take different parameters.
+     */
+    @Test
+    public void testCreateBridgeNode() {
+        //TODO
+    }
+    
+    @Test
+    public void testCreateControllerEntries() {
+      //TODO
+    }
+
+    @Test
+    public void testCreateGreTunnel() {
+      //TODO
+    }
+
+    @Test
+    public void testCreateMdsalProtocols() {
+      //TODO
+    }
+
+    @Test
+    public void testCreateOvsdbBridgeAugmentation() {
+      //TODO
+    }
+
+    /*
+     *  This test for 2 functions with the
+     *  same name that take different parameters.
+     */
+    @Test
+    public void testCreateOvsdbNode() {
+      //TODO
+    }
+
+    @Test
+    public void testCreateOvsdbNodeAugmentation() {
+      //TODO
+    }
+
+    @Test
+    public void testCreateOvsdbNodeId() {
+      //TODO
+    }
+
+    @Test
+    public void testCreateOvsdbTerminationPointAugmentation() {
+      //TODO
+    }
+
+    @Test
+    public void testCreateEvc() {
+      //TODO
+    }
+
+    @Test
+    public void testCreateUniNode() {
+      //TODO
+    }
+
+    @Test
+    public void testCreateUniNodeId() {
+      //TODO
+    }
+
+    /*
+     *  This test for 2 functions with the
+     *  same name that take different parameters.
+     */
+    @Test
+    public void testCreateTerminationPointNode() {
+      //TODO
+    }
+
+    @Test
+    public void testDelete() {
+      //TODO
+    }
+
+    @Test
+    public void testDeleteTerminationPoint() {
+      //TODO
+    }
+
+    @Test
+    public void testDeleteNode() {
+      //TODO
+    }
+
+    @Test
+    public void testExtract() {
+      //TODO
+    }
+
+    @Test
+    public void testExtractOriginal() {
+      //TODO
+    }
+
+    @Test
+    public void testExtractRemoved() {
+      //TODO
+    }
+
+    @Test
+    public void testFindOvsdbNode() {
+      //TODO
+    }
+
+    @Test
+    public void testFindUniNode() {
+      //TODO
+    }
+
+    @Test
+    public void testGetConnectionInfo() {
+      //TODO
+    }
+
+    @Test
+    public void testGetEvcLinks() {
+      //TODO
+    }
+
+    @Test
+    public void testGetLocalIp() {
+      //TODO
+    }
+
+    @Test
+    public void testGetOvsdbNodes() {
+      //TODO
+    }
+
+    /*
+     *  This test for 2 functions with the
+     *  same name that take different parameters.
+     */
+    @Test
+    public void testGetUniNodes() {
+      //TODO
+    }
+
+    @Test
+    public void testGetUnis() {
+      //TODO
+    }
+
+    @Test
+    public void testGetUni() {
+      //TODO
+    }
+
+    @Test
+    public void testRead() {
+      //TODO
+    }
+
+    @Test
+    public void testReadLink() {
+      //TODO
+    }
+    
+    @Test
+    public void testReadNode() {
+      //TODO
+    }
+
+    /*
+     *  This test for 2 functions with the
+     *  same name that take different parameters.
+     */
+    @Test
+    public void testUpdateUniNode() {
+      //TODO
+    }
+
+    @Test
+    public void testUpdateEvcNode() {
+      //TODO
+    }
+
+}
