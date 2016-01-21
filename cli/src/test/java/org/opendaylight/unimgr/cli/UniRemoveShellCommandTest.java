7440e7945057d7206f3605637a51ac6990e72424
Johann David <jdavid@inocybe.com>
2016-01-21 14:23:12 -0500

    Added unit tests for org.opendaylight.unimgr.cli package
    Change-Id: Ieab7cbc46fcd663ec0ce412706057d343604213c
    Signed-off-by: Johann David <jdavid@inocybe.com>

---
+++ cli/src/test/java/org/opendaylight/unimgr/cli/UniRemoveShellCommandTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2016 CableLabs and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.unimgr.cli;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.unimgr.api.IUnimgrConsoleProvider;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.unimgr.rev151012.Evc;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.unimgr.rev151012.EvcAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.unimgr.rev151012.UniAugmentation;
+import org.powermock.api.support.membermodification.MemberModifier;
+import org.powermock.reflect.Whitebox;
+
+public class UniRemoveShellCommandTest {
+    private final String ipAddress = "192.168.1.1";
+    private final UnimgrConsoleProviderTest provider = new UnimgrConsoleProviderTest();
+
+    @Mock UniRemoveShellCommand uniRemoveShellCommand;
+
+    @Before
+    public void setUp() throws IllegalArgumentException, IllegalAccessException{
+        uniRemoveShellCommand = mock(UniRemoveShellCommand.class, Mockito.CALLS_REAL_METHODS);
+        MemberModifier.field(UniRemoveShellCommand.class, "provider").set(uniRemoveShellCommand, provider);
+        MemberModifier.field(UniRemoveShellCommand.class, "ipAddress").set(uniRemoveShellCommand, ipAddress);
+    }
+
+    /**
+     * Test method for {@link org.opendaylight.unimgr.cli.UniRemoveShellCommand#doExecute()}.
+     * @throws Exception
+     */
+    @Test
+    public void testDoExecute() throws Exception {
+        final String success = "Uni successfully removed";
+        final String error = "Error removing Uni";
+        Object answer = Whitebox.invokeMethod(uniRemoveShellCommand, "doExecute");
+        assertEquals(success, answer);
+        answer = Whitebox.invokeMethod(uniRemoveShellCommand, "doExecute");
+        assertEquals(error, answer);
+    }
+
+}
