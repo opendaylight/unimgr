eb90a4db0be7756259acbb469c70f15aef0a5117
Johann David <jdavid@inocybe.com>
2016-01-21 14:23:12 -0500

    Added unit tests for org.opendaylight.unimgr.cli package
    Change-Id: Ieab7cbc46fcd663ec0ce412706057d343604213c
    Signed-off-by: Johann David <jdavid@inocybe.com>

---
+++ cli/src/test/java/org/opendaylight/unimgr/cli/UnimgrConsoleProviderTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2016 CableLabs and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.unimgr.cli;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.unimgr.api.IUnimgrConsoleProvider;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.unimgr.rev151012.Evc;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.unimgr.rev151012.EvcAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.unimgr.rev151012.UniAugmentation;
+
+public class UnimgrConsoleProviderTest implements IUnimgrConsoleProvider {
+
+        private final List<UniAugmentation> list = new ArrayList<UniAugmentation>();
+        private boolean firstTest = true;
+
+        @Override
+        public boolean addUni(UniAugmentation uni) {
+            return (list.contains(uni)) ? false : list.add(uni);
+        }
+
+        @Override
+        public boolean removeUni(IpAddress ipAddress) {
+            firstTest = !firstTest;
+            return !firstTest;
+        }
+
+        @Override
+        public List<UniAugmentation> listUnis(LogicalDatastoreType dataStoreType) {
+            return list;
+        }
+
+        public void setListUnis(int amount, String ipAddress) {
+            for(int i=0; i<amount; i++){
+                final UniAugmentation uniAug = mock(UniAugmentation.class);
+                final IpAddress ipAddr = mock(IpAddress.class);
+                final Ipv4Address ip4 = mock(Ipv4Address.class);
+                when(uniAug.getIpAddress()).thenReturn(ipAddr);
+                when(ipAddr.getIpv4Address()).thenReturn(ip4);
+                when(ip4.getValue()).thenReturn(ipAddress);
+                list.add(uniAug);
+            }
+        }
+
+        @Override
+        public UniAugmentation getUni(IpAddress ipAddress) {return list.get(0);}
+
+        @Override
+        public void close() throws Exception { }
+        @Override
+        public boolean removeEvc(String uuid) {return false;}
+        @Override
+        public Evc getEvc(String uuid) {return null;}
+        @Override
+        public boolean addEvc(EvcAugmentation evc) {return false;}
+
+}
