d44ccc5b5f2dbcac9abecef371f6d6c46468d53b
Johann David <jdavid@inocybe.com>
2016-02-04 10:56:11 -0500

    Created unit tests for the org.opendaylight.unimgr.command package
    Change-Id: I5760a5d8c0086b8d7c3f17fc9446ccc22b546a76
    Signed-off-by: Johann David <jdavid@inocybe.com>

---
+++ impl/src/test/java/org/opendaylight/unimgr/command/EvcDeleteCommandTest.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2016 CableLabs and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.unimgr.command;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.unimgr.impl.UnimgrMapper;
+import org.opendaylight.unimgr.impl.UnimgrUtils;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.unimgr.rev151012.EvcAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.unimgr.rev151012.evc.UniDest;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.unimgr.rev151012.evc.UniSource;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import com.google.common.base.Optional;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({UnimgrUtils.class})
+public class EvcDeleteCommandTest {
+
+    private EvcDeleteCommand evcDeleteCommand;
+    private AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> changes;
+    private DataBroker dataBroker;
+
+    @SuppressWarnings("unchecked")
+    @Before
+    public void setUp(){
+        PowerMockito.mockStatic(UnimgrUtils.class);
+        PowerMockito.mockStatic(UnimgrMapper.class);
+        changes = mock(AsyncDataChangeEvent.class);
+        dataBroker = mock(DataBroker.class);
+        evcDeleteCommand = new EvcDeleteCommand(dataBroker, changes);
+    }
+    /**
+     * Test method for {@link org.opendaylight.unimgr.command.EvcDeleteCommand#EvcDeleteCommand(org.opendaylight.controller.md.sal.binding.api.DataBroker, org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent)}.
+     * @throws Exception
+     */
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Test
+    public void testEvcDeleteCommand() throws Exception {
+        final Set<InstanceIdentifier<EvcAugmentation>> removedEvcs =
+                new HashSet<InstanceIdentifier<EvcAugmentation>>();
+        final List<UniSource> unisSource = new ArrayList<UniSource>();
+        final UniSource uniSource = mock(UniSource.class);
+        final List<UniDest> unisDest = new ArrayList<UniDest>();
+        final UniDest uniDest = mock(UniDest.class);
+        final EvcAugmentation evcAugmentation = mock(EvcAugmentation.class);
+        final Optional<Node> optionalNode = mock(Optional.class);
+        final InstanceIdentifier instanceOfNode = mock(InstanceIdentifier.class);
+        removedEvcs.add(mock(InstanceIdentifier.class));
+        unisSource.add(uniSource);
+        unisDest.add(uniDest);
+
+        when(evcAugmentation.getUniSource()).thenReturn(unisSource);
+        when(evcAugmentation.getUniDest()).thenReturn(unisDest);
+        when(uniSource.getUni()).thenReturn(instanceOfNode);
+        when(uniDest.getUni()).thenReturn(instanceOfNode);
+        when(UnimgrUtils.extractRemoved(any(AsyncDataChangeEvent.class), any(Class.class)))
+                .thenReturn(removedEvcs);
+        when(UnimgrUtils.read(any(DataBroker.class), any(LogicalDatastoreType.class),
+                any(InstanceIdentifier.class))).thenReturn(evcAugmentation);
+        when(UnimgrUtils.readNode(any(DataBroker.class), any(LogicalDatastoreType.class),
+                any(InstanceIdentifier.class))).thenReturn(optionalNode);
+        PowerMockito.doNothing().when(UnimgrUtils.class, "deleteEvcData",
+                any(DataBroker.class), any(Optional.class));
+        when(UnimgrUtils.deleteNode(any(DataBroker.class), any(InstanceIdentifier.class),
+                any(LogicalDatastoreType.class))).thenReturn(true);
+
+        evcDeleteCommand.execute();
+        PowerMockito.verifyStatic(times(2));
+        UnimgrUtils.deleteEvcData(any(DataBroker.class), any(Optional.class));
+        PowerMockito.verifyStatic(times(1));
+        UnimgrUtils.deleteNode(any(DataBroker.class), any(InstanceIdentifier.class),
+                any(LogicalDatastoreType.class));
+    }
+
+}
