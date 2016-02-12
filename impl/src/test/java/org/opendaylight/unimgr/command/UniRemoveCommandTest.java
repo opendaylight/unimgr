a2333f12f2c5f9036e924c7b8b20c85874991791
melserngawy <melserngawy@inocybe.com>
2016-02-12 16:56:46 -0500

    add abstract data tree change listener, clean up the code and Unit Tests
    Change-Id: I2671459b71f16f98f51f6c496718dc25bf0a8f4c
    Signed-off-by: melserngawy <melserngawy@inocybe.com>

--- impl/src/test/java/org/opendaylight/unimgr/command/UniRemoveCommandTest.java
+++ impl/src/test/java/org/opendaylight/unimgr/command/UniRemoveCommandTest.java
@@ -7,26 +7,19 @@
  */
 package org.opendaylight.unimgr.command;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.powermock.api.support.membermodification.MemberMatcher.method;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
-import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.unimgr.impl.UnimgrConstants;
 import org.opendaylight.unimgr.impl.UnimgrMapper;
@@ -34,7 +27,6 @@ import org.opendaylight.unimgr.utils.MdsalUtils;
 import org.opendaylight.unimgr.utils.OvsdbUtils;
 import org.opendaylight.unimgr.utils.UniUtils;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpAddress;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbNodeAugmentation;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbNodeRef;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.QosEntries;
@@ -42,20 +34,14 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.re
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.Queues;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.QueuesKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.unimgr.rev151012.UniAugmentation;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.unimgr.rev151012.UniAugmentationBuilder;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeBuilder;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
-import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.opendaylight.yangtools.yang.binding.InstanceIdentifier.InstanceIdentifierBuilder;
 import org.powermock.api.mockito.PowerMockito;
-import org.powermock.api.support.membermodification.MemberMatcher;
-import org.powermock.api.support.membermodification.MemberModifier;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -64,14 +50,13 @@ import com.google.common.base.Optional;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({UniUtils.class, OvsdbUtils.class, MdsalUtils.class,
         UnimgrMapper.class})
-public class UniDeleteCommandTest {
+public class UniRemoveCommandTest {
 
     private static final NodeId OVSDB_NODE_ID = new NodeId("ovsdb://7011db35-f44b-4aab-90f6-d89088caf9d8");
-    private static final String NODE_ID = "uni://10.0.0.1";
-
-    private UniDeleteCommand uniDeleteCommand;
-    private AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> changes;
+    private UniRemoveCommand uniRemoveCommand;
+    private DataTreeModification<Node> uni;
     private DataBroker dataBroker;
+    private Node uniNode;
 
     @SuppressWarnings("unchecked")
     @Before
@@ -80,20 +65,18 @@ public class UniDeleteCommandTest {
         PowerMockito.mockStatic(OvsdbUtils.class);
         PowerMockito.mockStatic(MdsalUtils.class);
         PowerMockito.mockStatic(UnimgrMapper.class);
-        changes = mock(AsyncDataChangeEvent.class);
         dataBroker = mock(DataBroker.class);
-        uniDeleteCommand = new UniDeleteCommand(dataBroker, changes);
+        uniNode = mock(Node.class);
+        uni = DataTreeModificationHelper.getUniNode(uniNode);
+        uniRemoveCommand = new UniRemoveCommand(dataBroker, uni);
     }
 
     /**
-     * Test method for {@link org.opendaylight.unimgr.command.UniDeleteCommand#execute()}.
+     * Test method for {@link org.opendaylight.unimgr.command.UniRemoveCommand#execute()}.
      */
     @SuppressWarnings({ "unchecked", "rawtypes"})
     @Test
     public void testExecute() {
-        final Set<InstanceIdentifier<UniAugmentation>> removedUnis =
-                new HashSet<InstanceIdentifier<UniAugmentation>>();
-        removedUnis.add(mock(InstanceIdentifier.class));
         final UniAugmentation uniAugmentation = mock(UniAugmentation.class);
         final OvsdbNodeRef ovsNodedRef = mock(OvsdbNodeRef.class);
         final Optional<Node> optionalNode = mock(Optional.class);
@@ -106,7 +89,7 @@ public class UniDeleteCommandTest {
         final OvsdbNodeAugmentation ovsdbNodeAugmentation = mock(OvsdbNodeAugmentation.class);
         final QosEntries qosEntries = mock(QosEntries.class);
         final Queues queues = mock(Queues.class);
-
+        when(uniNode.getAugmentation(UniAugmentation.class)).thenReturn(uniAugmentation);
         when(uniAugmentation.getIpAddress()).thenReturn(mock(IpAddress.class));
         when(uniAugmentation.getOvsdbNodeRef()).thenReturn(ovsNodedRef);
         when(ovsNodedRef.getValue()).thenReturn(uniKey);
@@ -121,8 +104,6 @@ public class UniDeleteCommandTest {
         when(queues.getKey()).thenReturn(mock(QueuesKey.class));
         queuesList.add(queues);
         when(ovsdbNodeAugmentation.getQueues()).thenReturn(queuesList);
-        when(OvsdbUtils.extractRemoved(any(AsyncDataChangeEvent.class), any(Class.class)))
-                .thenReturn(removedUnis);
         when(MdsalUtils.read(any(DataBroker.class), any(LogicalDatastoreType.class),
                 any(InstanceIdentifier.class))).thenReturn(uniAugmentation);
         when(MdsalUtils.readNode(any(DataBroker.class), any(LogicalDatastoreType.class),
@@ -138,9 +119,8 @@ public class UniDeleteCommandTest {
         when(UnimgrMapper.getOvsdbQueuesIid(any(Node.class), any(QueuesKey.class))).thenReturn(instanceOfNode);
         when(MdsalUtils.deleteNode(any(DataBroker.class), any(InstanceIdentifier.class),
                 any(LogicalDatastoreType.class))).thenReturn(true);
-        uniDeleteCommand.execute();
-
-        PowerMockito.verifyStatic(times(5));
+        uniRemoveCommand.execute();
+        PowerMockito.verifyStatic(times(4));
         MdsalUtils.deleteNode(any(DataBroker.class), any(InstanceIdentifier.class),
                 any(LogicalDatastoreType.class));
     }
