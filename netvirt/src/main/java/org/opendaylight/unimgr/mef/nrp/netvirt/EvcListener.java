51e4b2b58093e54c73828bd51f38a8a3fdbc061e
Yakir Dorani <yakir.dorani@hpe.com>
2016-06-26 15:17:07 +0300

    Add netvirt bundle
    Change-Id: I3cf6a9220af214989474f78e525d42794139b2da
    Signed-off-by: Yakir Dorani <yakir.dorani@hpe.com>

---
+++ netvirt/src/main/java/org/opendaylight/unimgr/mef/nrp/netvirt/EvcListener.java
@@ -0,0 +1,182 @@
+/*
+ * Copyright (c) 2016 Hewlett Packard Enterprise, Co. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.unimgr.mef.nrp.netvirt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.genius.mdsalutil.AbstractDataChangeListener;
+import org.opendaylight.genius.mdsalutil.MDSALUtil;
+import org.opendaylight.yang.gen.v1.http.metroethernetforum.org.ns.yang.mef.services.rev150526.MefServices;
+import org.opendaylight.yang.gen.v1.http.metroethernetforum.org.ns.yang.mef.services.rev150526.mef.services.MefService;
+import org.opendaylight.yang.gen.v1.http.metroethernetforum.org.ns.yang.mef.services.rev150526.mef.services.mef.service.Evc;
+import org.opendaylight.yang.gen.v1.http.metroethernetforum.org.ns.yang.mef.services.rev150526.mef.services.mef.service.evc.unis.Uni;
+import org.opendaylight.yang.gen.v1.http.metroethernetforum.org.ns.yang.mef.services.rev150526.mef.services.mef.service.evc.unis.uni.EvcUniCeVlans;
+import org.opendaylight.yang.gen.v1.http.metroethernetforum.org.ns.yang.mef.services.rev150526.mef.services.mef.service.evc.unis.uni.evc.uni.ce.vlans.EvcUniCeVlan;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.PhysAddress;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.ElanInstances;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.ElanInterfaces;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.instances.ElanInstance;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.instances.ElanInstanceBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.instances.ElanInstanceKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.interfaces.ElanInterface;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.interfaces.ElanInterfaceBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.elan.interfaces.ElanInterfaceKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.elan.rev150602.forwarding.entries.MacEntry;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netvirt.neutronvpn.rev150602.DeleteL3VPNInput;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Optional;
+
+public class EvcListener extends AbstractDataChangeListener<Evc> {
+
+    private static final Logger log = LoggerFactory.getLogger(EvcListener.class);
+    private DataBroker dataBroker;
+    private ListenerRegistration<DataChangeListener> evcListenerRegistration;
+
+    public EvcListener(final DataBroker dataBroker) {
+        super(Evc.class);
+        this.dataBroker = dataBroker;
+
+        registerListener();
+    }
+
+    public void registerListener() {
+        try {
+            evcListenerRegistration = dataBroker.registerDataChangeListener(LogicalDatastoreType.CONFIGURATION, getWildCardPath(), EvcListener.this,
+                    DataChangeScope.SUBTREE);
+        } catch (final Exception e) {
+            log.error("Evc DataChange listener registration failed !", e);
+            throw new IllegalStateException("Evc registration Listener failed.", e);
+        }
+    }
+
+    private InstanceIdentifier<Evc> getWildCardPath() {
+        InstanceIdentifier<Evc> instanceIdentifier = InstanceIdentifier.create(MefServices.class).child(MefService.class).child(Evc.class);
+
+        return instanceIdentifier;
+    }
+
+    @Override
+    protected void remove(InstanceIdentifier<Evc> identifier, Evc del) {
+        removeEvc(del);
+    }
+
+    private void removeEvc(Evc del) {
+        try {
+            String instanceName = del.getEvcId().getValue();
+            
+            log.info("Removing Elan Instance: " + instanceName);
+
+            NetvirtUtils.deleteElanInstance(dataBroker, instanceName);
+        } catch (final Exception e) {
+            log.error("Remove evc failed !", e);
+            throw new IllegalStateException("Remove evc failed.", e);
+        }
+    }
+
+    private void removeUni(String instanceName, Uni uni) {
+        String uniId = uni.getUniId().getValue();
+        EvcUniCeVlans evcUniCeVlans = uni.getEvcUniCeVlans();
+
+        if (evcUniCeVlans != null && !evcUniCeVlans.getEvcUniCeVlan().isEmpty()) {
+            for (EvcUniCeVlan x : evcUniCeVlans.getEvcUniCeVlan()) {
+
+                String interfaceName = NetvirtUtils.getInterfaceNameForVlan(uniId, x.getVid().toString());
+                log.info("Removing elan interface: " + interfaceName);
+                NetvirtUtils.deleteElanInterface(dataBroker, instanceName, interfaceName);
+            }
+        } else {
+            log.info("Removing elan interface: " + uniId);
+            NetvirtUtils.deleteElanInterface(dataBroker, instanceName, uniId);
+        }
+    }
+
+    @Override
+    protected void update(InstanceIdentifier<Evc> identifier, Evc original, Evc update) {
+        try {
+            String instanceName = update.getEvcId().getValue();
+
+            log.info("Updating elan instance: " + instanceName);
+            
+            List<Uni> originalUni = original.getUnis().getUni();
+            List<Uni> updateUni = update.getUnis().getUni();
+            if (updateUni != null && !updateUni.isEmpty()) {
+                List<Uni> existingClonedUni = new ArrayList<>();
+                if (originalUni != null && !originalUni.isEmpty()) {
+                    existingClonedUni.addAll(0, originalUni);
+                    originalUni.removeAll(updateUni);
+                    updateUni.removeAll(existingClonedUni);
+                    // removing the Uni which are not presented in the updated
+                    // List
+                    for (Uni uni : originalUni) {
+                        removeUni(instanceName, uni);
+                    }
+                }
+
+                // Adding the new Uni which are presented in the updated List
+                if (updateUni.size() > 0) {
+                    for (Uni uni : updateUni) {
+                        createUni(instanceName, uni);
+                    }
+                }
+            } else if (originalUni != null && !originalUni.isEmpty()) {
+                for (Uni uni : originalUni) {
+                    removeUni(instanceName, uni);
+                }
+            }
+        } catch (final Exception e) {
+            log.error("Update evc failed !", e);
+            throw new IllegalStateException("Update evc failed.", e);
+        }
+    }
+
+    @Override
+    protected void add(InstanceIdentifier<Evc> identifier, Evc add) {
+        try {
+            String instanceName = add.getEvcId().getValue();
+            
+            log.info("Adding elan instance: " + instanceName);
+            NetvirtUtils.createElanInstance(dataBroker, instanceName);
+
+            // Create elan interfaces
+            for (Uni uni : add.getUnis().getUni()) {
+                createUni(instanceName, uni);
+            }
+        } catch (final Exception e) {
+            log.error("Add evc failed !", e);
+            throw new IllegalStateException("Add evc failed.", e);
+        }
+    }
+
+    private void createUni(String instanceName, Uni uni) {
+        String uniId = uni.getUniId().getValue();
+        EvcUniCeVlans evcUniCeVlans = uni.getEvcUniCeVlans();
+
+        if (evcUniCeVlans != null && !evcUniCeVlans.getEvcUniCeVlan().isEmpty()) {
+            for (EvcUniCeVlan x : evcUniCeVlans.getEvcUniCeVlan()) {
+
+                String interfaceName = NetvirtUtils.getInterfaceNameForVlan(uniId, x.getVid().toString());
+                
+                log.info("Adding elan interface: " + interfaceName);
+                NetvirtUtils.createElanInterface(dataBroker, instanceName, interfaceName);
+            }
+        } else {
+            log.info("Adding elan interface: " + uniId);
+            NetvirtUtils.createElanInterface(dataBroker, instanceName, uniId);
+        }
+    }
+}
