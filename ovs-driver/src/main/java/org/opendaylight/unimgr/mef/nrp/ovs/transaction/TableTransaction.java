48d3453510c1090e39f72548f187034ee3d82bb9
Marek Ryznar <marek.ryznar@amartus.com>
2017-02-13 14:14:45 +0100

    Addition of new module - ovs-driver,  responsible for creating forwarding constructs in Open vSwitch devices.
    Developer's Certificate of Origin 1.1
    
            By making a contribution to this project, I certify that:
    
            (a) The contribution was created in whole or in part by me and I
                have the right to submit it under the open source license
                indicated in the file; or
    
            (b) The contribution is based upon previous work that, to the best
                of my knowledge, is covered under an appropriate open source
                license and I have the right under that license to submit that
                work with modifications, whether created in whole or in part
                by me, under the same open source license (unless I am
                permitted to submit under a different license), as indicated
                in the file; or
    
            (c) The contribution was provided directly to me by some other
                person who certified (a), (b) or (c) and I have not modified
                it.
    
            (d) I understand and agree that this project and the contribution
                are public and that a record of the contribution (including all
                personal information I submit with it, including my sign-off) is
                maintained indefinitely and may be redistributed consistent with
                this project or the open source license(s) involved.
    
    Change-Id: Ieecc38e0716822932a2a61d5ed4125f4d82e0771
    Signed-off-by: Marek Ryznar <marek.ryznar@amartus.com>

---
+++ ovs-driver/src/main/java/org/opendaylight/unimgr/mef/nrp/ovs/transaction/TableTransaction.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2016 Cisco Systems Inc and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.unimgr.mef.nrp.ovs.transaction;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.Table;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.TableKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.Nodes;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+
+/**
+ * Performs output (write/delete) transactions related to openflow flows
+ * during OvsDriver activation/deactivation.
+ *
+ * @author jakub.niezgoda@amartus.com
+ */
+public class TableTransaction {
+
+    private DataBroker dataBroker;
+    private InstanceIdentifier<Table> tableInstanceId;
+
+    private static final Logger LOG = LoggerFactory.getLogger(TableTransaction.class);
+
+    /**
+     * Creates and initialize TableTransaction object
+     *
+     * @param dataBroker access to data tree store
+     * @param node       openflow network node
+     * @param table      flow table of node
+     */
+    public TableTransaction(DataBroker dataBroker, Node node, Table table) {
+        this.dataBroker = dataBroker;
+        this.tableInstanceId = getTableIid(node.getKey(), table.getKey());
+    }
+
+    /**
+     * Writes flows to the flow table
+     *
+     * @param flows list of flows to be added
+     * @throws TransactionCommitFailedException if writing flows transaction fails
+     */
+    public void writeFlows(List<Flow> flows) throws TransactionCommitFailedException {
+        for (Flow flow : flows) {
+            writeFlow(flow);
+        }
+    }
+
+    /**
+     * Writes flow to the flow table
+     *
+     * @param flow flow to be added
+     * @throws TransactionCommitFailedException if writing flow transaction fails
+     */
+    public void writeFlow(Flow flow) throws TransactionCommitFailedException {
+        WriteTransaction transaction = dataBroker.newWriteOnlyTransaction();
+        LOG.debug("Writing flow '" + flow.getId().getValue() + "' to " + LogicalDatastoreType.CONFIGURATION + " data store.");
+        transaction.put(LogicalDatastoreType.CONFIGURATION, getFlowIid(flow), flow, true);
+        transaction.submit().checkedGet();
+    }
+
+    /**
+     * Deletes flows from the flow table
+     *
+     * @param flows list of flows to be deleted
+     * @param writeToConfigurationDataStoreFirst if set flows are written to CONFIGURATION data store before deletion
+     * @throws TransactionCommitFailedException if writing/deleting flows transaction fails
+     */
+    public void deleteFlows(List<Flow> flows, boolean writeToConfigurationDataStoreFirst) throws TransactionCommitFailedException {
+        if (writeToConfigurationDataStoreFirst) {
+            writeFlows(flows);
+        }
+        for (Flow flow : flows) {
+            deleteFlow(flow);
+        }
+    }
+
+    /**
+     * Deletes flow from the flow table
+     *
+     * @param flow flow to be deleted
+     * @throws TransactionCommitFailedException if deleting flow transaction fails
+     */
+    public void deleteFlow(Flow flow) throws TransactionCommitFailedException {
+        WriteTransaction deleteTransaction = dataBroker.newWriteOnlyTransaction();
+        LOG.debug("Deleting flow '" + flow.getId().getValue() + "' from " + LogicalDatastoreType.CONFIGURATION + " data store.");
+        deleteTransaction.delete(LogicalDatastoreType.CONFIGURATION, getFlowIid(flow));
+        deleteTransaction.submit().checkedGet();
+    }
+
+    private InstanceIdentifier<Table> getTableIid(NodeKey nodeKey, TableKey tableKey){
+        return InstanceIdentifier.builder(Nodes.class)
+                                 .child(Node.class, nodeKey)
+                                 .augmentation(FlowCapableNode.class)
+                                 .child(Table.class, tableKey)
+                                 .build();
+    }
+
+    private InstanceIdentifier<Flow> getFlowIid(Flow flow){
+        return tableInstanceId.child(Flow.class, flow.getKey());
+    }
+}
---
+++ ovs-driver/src/main/java/org/opendaylight/unimgr/mef/nrp/ovs/util/OpenFlowUtils.java
@@ -0,0 +1,251 @@
+/*
+ * Copyright (c) 2016 Cisco Systems Inc and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.unimgr.mef.nrp.ovs.util;
+
+import org.opendaylight.unimgr.mef.nrp.common.ResourceNotAvailableException;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.action.types.rev131112.action.list.Action;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowCapableNode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.FlowId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.Table;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.Flow;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.FlowBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.inventory.rev130819.tables.table.FlowKey;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.flow.types.rev131026.flow.Match;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Link;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Class responsible for managing OpenFlow rules in OVS.
+ *
+ * @author marek.ryznar@amartus.com
+ */
+public class OpenFlowUtils {
+    private static final Logger LOG = LoggerFactory.getLogger(OpenFlowUtils.class);
+    private static final String INTERSWITCH_FLOW_ID_PREFIX = "interswitch";
+    private static final String FLOW_ID_MIDDLE_PART = "vlan";
+    private static final String DROP_FLOW_ID = "default-DROP";
+    private static final Short FLOW_TABLE_ID = 0;
+    private static final int VLAN_FLOW_PRIORITY = 20;
+    private static final int INTERSWITCH_FLOW_PRIORITY = 10;
+    private static final int DROP_FLOW_PRIORITY = 0;
+
+    private static final String FLOW_TABLE_NOT_PRESENT_ERROR_MESSAGE = "Flow table is not present in node '%s'.";
+    private static final String NODE_NOT_AUGMENTED_ERROR_MESSAGE = "Node '%s' does not have '%s' augmentation.";
+    /**
+     * Checks if flow table contains base flows
+     *
+     * @param table - openflow node's table
+     * @return true if table is preconfigured, false in other case
+     */
+    public static boolean isTablePreconfigured(Table table) {
+        boolean isTablePreconfigured = false;
+
+        for (Flow flow : table.getFlow()) {
+            if (flow.getId().getValue().startsWith(INTERSWITCH_FLOW_ID_PREFIX)) {
+                isTablePreconfigured = true;
+                break;
+            }
+        }
+
+        LOG.debug("Table is " + (isTablePreconfigured ? "" : "not") + " preconfigured with default flows.");
+        return isTablePreconfigured;
+    }
+
+    /**
+     * Returns flow table for the specified node
+     *
+     * @param node openflow node
+     * @return flow table
+     * @throws ResourceNotAvailableException if node is not augmented with FlowCapableNode class or flow table is not present in node
+     */
+    public static Table getTable(Node node) throws ResourceNotAvailableException {
+        String nodeId = node.getId().getValue();
+        FlowCapableNode flowCapableNode = node.getAugmentation(FlowCapableNode.class);
+        if (flowCapableNode == null) {
+            LOG.warn(String.format(NODE_NOT_AUGMENTED_ERROR_MESSAGE, nodeId, FlowCapableNode.class.toString()));
+            throw new ResourceNotAvailableException(String.format(NODE_NOT_AUGMENTED_ERROR_MESSAGE, nodeId, FlowCapableNode.class.toString()));
+        }
+
+        Optional<Table> flowTable = flowCapableNode.getTable()
+                                                   .stream()
+                                                   .filter(table -> table.getId().equals(FLOW_TABLE_ID))
+                                                   .findFirst();
+        if (!flowTable.isPresent()) {
+            LOG.warn(String.format(FLOW_TABLE_NOT_PRESENT_ERROR_MESSAGE, nodeId));
+            throw new ResourceNotAvailableException(String.format(FLOW_TABLE_NOT_PRESENT_ERROR_MESSAGE, nodeId));
+        }
+
+        return flowTable.get();
+    }
+
+    /**
+     * Returns list of flows for passing traffic with given VLAN ID
+     *
+     * @param servicePort port on which service is activated (format: openflow:[node]:[port])
+     * @param externalVlanId VLAN ID used outside OvSwitch network
+     * @param internalVlanId VLAN ID used internally in OvSwitch network
+     * @param interswitchLinks list of interswitch links for the node on which service is activated
+     * @param serviceName service name (used as prefix for flow IDs)
+     * @return list of flows
+     */
+    public static List<Flow> getVlanFlows(String servicePort, int externalVlanId, int internalVlanId, List<Link> interswitchLinks, String serviceName) {
+        List<Flow> flows = new ArrayList<>();
+        flows.addAll(createVlanPassingFlows(servicePort, externalVlanId, internalVlanId, serviceName, interswitchLinks));
+        flows.add(createVlanIngressFlow(servicePort, externalVlanId, internalVlanId, serviceName, interswitchLinks));
+
+        return flows;
+    }
+
+    /**
+     * Returns list of flows related to service named serviceName installed in specified flow table
+     *
+     * @param table flow table
+     * @param serviceName service name
+     * @return list of flows
+     */
+    public static List<Flow> getServiceFlows(Table table, String serviceName) {
+        return table.getFlow().stream()
+                              .filter(flow -> flow.getId().getValue().startsWith(serviceName))
+                              .collect(Collectors.toList());
+    }
+
+    /**
+     * Returns list of flows - full-mesh - created on the base of list of provided links and default dropping all flow
+     *
+     * @param interswitchLinks list of links
+     * @return list of flows
+     */
+    public static List<Flow> getBaseFlows(List<Link> interswitchLinks) {
+        List<Flow> baseFlows = new ArrayList<>();
+
+        baseFlows.addAll(createInterswitchFlows(interswitchLinks));
+        baseFlows.add(createDefaultFlow());
+
+        return baseFlows;
+    }
+
+    /**
+     * Returns list of flows installed in flow table
+     *
+     * @param table flow table
+     * @return list of flows
+     */
+    public static List<Flow> getExistingFlows(Table table) {
+        return table.getFlow().stream().map(x -> new FlowBuilder(x).build()).collect(Collectors.toList());
+    }
+
+    private static List<Flow> createInterswitchFlows(List<Link> interswitchLinks) {
+        List<Flow> flows = new ArrayList<>();
+
+        // ports have the same names as interswitch links
+        List<String> portIds = interswitchLinks.stream()
+                                               .map(link -> link.getLinkId().getValue())
+                                               .collect(Collectors.toList());
+
+        for (String portId : portIds) {
+            List<Action> outputActions = portIds.stream()
+                                                .filter(outputPortId -> !outputPortId.equalsIgnoreCase(portId))
+                                                .map(outputPortId -> ActionUtils.createOutputAction(outputPortId, portIds.indexOf(outputPortId)))
+                                                .collect(Collectors.toList());
+            FlowId flowId = new FlowId(INTERSWITCH_FLOW_ID_PREFIX + "-" + portId);
+            Flow flow = new FlowBuilder().setId(flowId)
+                                         .setKey(new FlowKey(flowId))
+                                         .setTableId(FLOW_TABLE_ID)
+                                         .setPriority(INTERSWITCH_FLOW_PRIORITY)
+                                         .setMatch(MatchUtils.createInPortMatch(portId))
+                                         .setInstructions(ActionUtils.createInstructions(outputActions))
+                                         .build();
+            flows.add(flow);
+        }
+
+        return flows;
+    }
+
+    private static Flow createDefaultFlow() {
+        FlowId dropFlowId = new FlowId(DROP_FLOW_ID);
+        return new FlowBuilder().setId(dropFlowId)
+                                .setKey(new FlowKey(dropFlowId))
+                                .setTableId(FLOW_TABLE_ID)
+                                .setPriority(DROP_FLOW_PRIORITY)
+                                .setInstructions(ActionUtils.createInstructions(Arrays.asList(ActionUtils.createDropAction(0))))
+                                .build();
+    }
+
+    private static List<Flow> createVlanPassingFlows(String outputPort, int externalVlanId, int internalVlanId, String serviceName, List<Link> interswitchLinks) {
+        return interswitchLinks.stream()
+                               .map(link -> createVlanPassingFlow(outputPort, link.getLinkId().getValue(), externalVlanId, internalVlanId, serviceName))
+                               .collect(Collectors.toList());
+    }
+
+    private static Flow createVlanPassingFlow(String outputPort, String inputPort, int externalVlanId, int internalVlanId, String serviceName) {
+        // Create list of actions and VLAN match
+        List<Action> actions = new ArrayList<>();
+        Match vlanMatch;
+        int actionOrder = 0;
+        if (externalVlanId == internalVlanId) {
+            vlanMatch = MatchUtils.createVlanMatch(externalVlanId, inputPort);
+        } else {
+            vlanMatch = MatchUtils.createVlanMatch(internalVlanId, inputPort);
+            actions.add(ActionUtils.createPopVlanAction(actionOrder++));
+            actions.add(ActionUtils.createPushVlanAction(actionOrder++));
+            actions.add(ActionUtils.createSetVlanIdAction(externalVlanId, actionOrder++));
+        }
+        actions.add(ActionUtils.createOutputAction(outputPort, actionOrder));
+
+        FlowId flowId = new FlowId(getVlanFlowId(serviceName, inputPort));
+        return new FlowBuilder().setId(flowId)
+                .setKey(new FlowKey(flowId))
+                .setTableId(FLOW_TABLE_ID)
+                .setPriority(VLAN_FLOW_PRIORITY)
+                .setMatch(vlanMatch)
+                .setInstructions(ActionUtils.createInstructions(actions))
+                .build();
+    }
+
+    private static Flow createVlanIngressFlow(String inputPort, int externalVlanId, int internalVlanId, String serviceName, List<Link> interswitchLinks) {
+        // Create list of output port IDs
+        List<String> outputPortIds = interswitchLinks.stream()
+                                                     .map(link -> link.getLinkId().getValue())
+                                                     .filter(outputPort -> !outputPort.equalsIgnoreCase(inputPort))
+                                                     .collect(Collectors.toList());
+
+        // Create list of actions
+        List<Action> actions = new ArrayList<>();
+        int actionOrder = 0;
+        // 1. Create VLAN actions performing VLAN translation when service VLAN is already used in OvSwitch network
+        if (externalVlanId != internalVlanId) {
+            actions.add(ActionUtils.createPopVlanAction(actionOrder++));
+            actions.add(ActionUtils.createPushVlanAction(actionOrder++));
+            actions.add(ActionUtils.createSetVlanIdAction(internalVlanId, actionOrder++));
+        }
+        // 2. Create output actions
+        final int outputActionOrder = actionOrder;
+        actions.addAll(outputPortIds.stream()
+                                    .map(outputPortId -> ActionUtils.createOutputAction(outputPortId,
+                                                                                        outputActionOrder + outputPortIds.indexOf(outputPortId)))
+                                    .collect(Collectors.toList()));
+
+        FlowId flowId = new FlowId(getVlanFlowId(serviceName, inputPort));
+        return new FlowBuilder().setId(flowId)
+                                .setKey(new FlowKey(flowId))
+                                .setTableId(FLOW_TABLE_ID)
+                                .setPriority(VLAN_FLOW_PRIORITY)
+                                .setMatch(MatchUtils.createVlanMatch(externalVlanId, inputPort))
+                                .setInstructions(ActionUtils.createInstructions(actions))
+                                .build();
+    }
+
+    private static String getVlanFlowId(String serviceName, String inputPort) {
+        return serviceName + "-" + FLOW_ID_MIDDLE_PART + "-" + inputPort;
+    }
+}
