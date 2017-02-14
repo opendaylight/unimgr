df1d8163b8ee0632eb1b0aa907727005f3b49fc6
Marek Ryznar <marek.ryznar@amartus.com>
2017-02-14 10:28:44 +0100

    Addition of module - cisco-xe-driver.
    The cisco-xe-driver is responsible for creating forwarding constructs
    in cisco devices with IOS XE Operating System.
    
    Developer's Certificate of Origin 1.1
    
            By making a contribution to this project, I certify that:
    
            (a) The contribution was created in whole or in part by me and I
                have the right to submit it under the open source license
                indicated in the file; or
    
            (b) The contribution is based upon previous work that, to the best
                of my knowledge, is covered under an appropriate open source
                license and I have the right under that license to submit that
                work with modifications, whether created in whole or in part
                by me, under the same open source license (unless I am
                permitted to submit under a different license), as indicated
                in the file; or
    
            (c) The contribution was provided directly to me by some other
                person who certified (a), (b) or (c) and I have not modified
                it.
    
            (d) I understand and agree that this project and the contribution
                are public and that a record of the contribution (including all
                personal information I submit with it, including my sign-off) is
                maintained indefinitely and may be redistributed consistent with
                this project or the open source license(s) involved.
    
    Change-Id: I290c14046969dfa3f2b597a78376a4368da50045
    Signed-off-by: Marek Ryznar <marek.ryznar@amartus.com>
    Signed-off-by: Donald Hunter <donaldh@cisco.com>

---
+++ cisco-xe-driver/src/main/java/org/opendaylight/unimgr/mef/nrp/cisco/rest/response/AuthResponse.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2016 Cisco Systems Inc and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.unimgr.mef.nrp.cisco.rest.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
+public class AuthResponse {
+
+	@JsonProperty("token-id")
+	private String tokenId;
+
+	public String getTokenId() {
+		return tokenId;
+	}
+
+}
