2e003b8f2cb48d2ef250a23e8eb1958d899a8513
santanude <santanu.de@xoriant.com>
2020-07-28 09:03:10 +0200

    Multiple version of Cisco Xr Driver
    Signed-off-by: santanude <santanu.de@xoriant.com>
    Change-Id: I5f59962880503a35bc4564a8fbb2058be451c6cc
    Signed-off-by: Om Awasthi <om.SAwasthi@xoriant.com>
    Signed-off-by: Arif Hussain <om.SAwasthi@xoriant.com>

---
+++ cisco-xr-driver/cisco-xr-driver-6.5/src/test/java/org/opendaylight/unimgr/mef/nrp/cisco/xr/l2vpn/helper/BridgeDomainHelperTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2016 Cisco Systems Inc and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.unimgr.mef.nrp.cisco.xr.l2vpn.helper;
+
+/*
+ * @author Om.SAwasthi@Xoriant.Com
+ *
+ */
+public class BridgeDomainHelperTest {
+	/*
+	 * private BridgeDomainHelper bridgeDomainHelper;
+	 * 
+	 * @Before public void setUp() throws Exception { bridgeDomainHelper = new
+	 * BridgeDomainHelper(); }
+	 * 
+	 * @SuppressWarnings("deprecation")
+	 * 
+	 * @Test public void createBridgeDomainGroupsTest() { BridgeDomainGroup
+	 * bridgeDomainGroup = PowerMockito.mock(BridgeDomainGroup.class);
+	 * BridgeDomainGroups bridgeDoaminGroups =
+	 * BridgeDomainHelper.createBridgeDomainGroups(bridgeDomainGroup);
+	 * Assert.assertNotNull(bridgeDoaminGroups);
+	 * 
+	 * }
+	 * 
+	 * @SuppressWarnings("deprecation")
+	 * 
+	 * @Test public void appendBridgeDomainTest() { String name = "ciscoD2";
+	 * BdAttachmentCircuits bdattachmentCircuits =
+	 * PowerMockito.mock(BdAttachmentCircuits.class); BdPseudowires bdpseudowires =
+	 * PowerMockito.mock(BdPseudowires.class); BridgeDomainHelper
+	 * bridgeDomainHelper1 = bridgeDomainHelper.appendBridgeDomain(name,
+	 * bdattachmentCircuits, bdpseudowires);
+	 * Assert.assertNotNull(bridgeDomainHelper1); }
+	 * 
+	 * @SuppressWarnings("deprecation")
+	 * 
+	 * @Test public void buildTest() { BridgeDomainGroup bridgeDomainGroups =
+	 * bridgeDomainHelper.build("ciscoD2");
+	 * Assert.assertNotNull(bridgeDomainGroups); }
+	 */
+
+}
