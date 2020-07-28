45fd3ba22ca381dda36da2117374a6f449d520ba
santanude <santanu.de@xoriant.com>
2020-07-28 09:03:10 +0200

    Multiple version of Cisco Xr Driver
    Signed-off-by: santanude <santanu.de@xoriant.com>
    Change-Id: I5f59962880503a35bc4564a8fbb2058be451c6cc
    Signed-off-by: Om Awasthi <om.SAwasthi@xoriant.com>
    Signed-off-by: Arif Hussain <om.SAwasthi@xoriant.com>
    Signed-off-by: santanude <santanu.de@xoriant.com>

---
+++ cisco-xr-driver/cisco-xr-driver-6.5/src/test/java/org/opendaylight/unimgr/mef/nrp/cisco/xr/six/five/common/util/LoopbackUtilsTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2016 Cisco Systems Inc and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.unimgr.mef.nrp.cisco.xr.six.five.common.util;
+
+import org.opendaylight.mdsal.binding.dom.adapter.test.AbstractConcurrentDataBrokerTest;
+
+
+/*
+ * @author marek.ryznar@amartus.com
+ */
+public class LoopbackUtilsTest extends AbstractConcurrentDataBrokerTest {
+	/*
+	 * private static final Logger LOG =
+	 * LoggerFactory.getLogger(LoopbackUtilsTest.class); private DataBroker broker;
+	 * private static String nodeName = "192.168.2.1"; private static String
+	 * topoName = "topo://"; private static String loopbackAddress = "192.168.2.20";
+	 * private static String portNumber = "8080";
+	 * 
+	 * @Before public void setUp() { broker = getDataBroker(); }
+	 * 
+	 * @After public void clean() { deleteNode(LoopbackUtils.getNodeIid(new
+	 * NodeId(nodeName),new TopologyId(topoName))); }
+	 * 
+	 * @Test public void testLoopbackAddress() { //given ServicePort port =
+	 * L2vpnTestUtils.port(topoName, nodeName, portNumber);
+	 * createAndPersistNode(true);
+	 * 
+	 * //when Ipv4AddressNoZone ipv4AddressNoZone =
+	 * LoopbackUtils.getIpv4Address(port, broker);
+	 * 
+	 * //then assertEquals(loopbackAddress,ipv4AddressNoZone.getValue()); }
+	 * 
+	 * @Test public void testAbsenceOfLoopbackAddress() { //given ServicePort port =
+	 * L2vpnTestUtils.port(topoName, nodeName, portNumber);
+	 * createAndPersistNode(false);
+	 * 
+	 * //when Ipv4AddressNoZone ipv4AddressNoZone =
+	 * LoopbackUtils.getIpv4Address(port, broker);
+	 * 
+	 * //then
+	 * assertEquals(LoopbackUtils.getDefaultLoopback(),ipv4AddressNoZone.getValue())
+	 * ; }
+	 * 
+	 * private void createAndPersistNode(boolean ifLoopbackAddress) { NodeId nodeId
+	 * = new NodeId(nodeName); Node node = createNode(nodeId,ifLoopbackAddress);
+	 * InstanceIdentifier<Node> nodeIid = writeNode(node); }
+	 * 
+	 * private Node createNode(NodeId nodeId,boolean ifLoopbackAddress) {
+	 * NodeBuilder nodeBuilder = new NodeBuilder();
+	 * 
+	 * NodeId nodeIdTopo = new NodeId(topoName); NodeKey nodeKey = new
+	 * NodeKey(nodeIdTopo);
+	 * 
+	 * nodeBuilder.setNodeId(nodeId);
+	 * 
+	 * if (ifLoopbackAddress) { Ipv4Address ipv4Address = new
+	 * Ipv4Address(loopbackAddress); IpAddress ipAddress = new
+	 * IpAddress(ipv4Address); LoopbackAugmentationBuilder
+	 * loopbackAugmentationBuilder = new LoopbackAugmentationBuilder();
+	 * loopbackAugmentationBuilder.setLoopbackAddress(ipAddress);
+	 * nodeBuilder.addAugmentation(LoopbackAugmentation.class,
+	 * loopbackAugmentationBuilder.build()); }
+	 * 
+	 * return nodeBuilder.build(); }
+	 * 
+	 * private InstanceIdentifier<Node> writeNode(Node node) {
+	 * InstanceIdentifier<Node> nodeInstanceId =
+	 * LoopbackUtils.getNodeIid(node.getNodeId(),new TopologyId(topoName));
+	 * WriteTransaction transaction = broker.newWriteOnlyTransaction();
+	 * 
+	 * transaction.put(LogicalDatastoreType.CONFIGURATION, nodeInstanceId,
+	 * node,true); try { FluentFuture<? extends CommitInfo> future =
+	 * transaction.commit(); future.get(); return nodeInstanceId; } catch
+	 * (InterruptedException | ExecutionException e) {
+	 * LOG.error("Unable to write node with Iid {} to store {}.", nodeInstanceId,
+	 * LogicalDatastoreType.CONFIGURATION); } return null; }
+	 * 
+	 * private void deleteNode(InstanceIdentifier<Node> nodeIid) { WriteTransaction
+	 * transaction = broker.newWriteOnlyTransaction();
+	 * transaction.delete(LogicalDatastoreType.CONFIGURATION, nodeIid); try {
+	 * transaction.commit().get(); } catch (InterruptedException |
+	 * ExecutionException e) {
+	 * LOG.error("Unable to remove node with Iid {} from store {}.", nodeIid,
+	 * LogicalDatastoreType.CONFIGURATION); } }
+	 */
+}
---
+++ cisco-xr-driver/cisco-xr-driver-6.5/src/test/java/org/opendaylight/unimgr/mef/nrp/cisco/xr/six/five/l2vpn/helper/BridgeDomainAttachmentCircuitHelperTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2018 Xoriant Corporation and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.unimgr.mef.nrp.cisco.xr.six.five.l2vpn.helper;
+
+/*
+ * @author Om.SAwasthi@Xoriant.Com
+ *
+ */
+public class BridgeDomainAttachmentCircuitHelperTest {
+
+	/*
+	 * private BridgeDomainAttachmentCircuitHelper bdAttachmentCircuitHelper;
+	 * private EndPoint ep1; private ServicePort port; private static final String
+	 * UUID1 = "sip:ciscoD1:GigabitEthernet0/0/0/1"; private static final String
+	 * NETCONF_TOPOLODY_NAME = "topology-netconf";
+	 * 
+	 * @Before public void setUp() throws Exception { bdAttachmentCircuitHelper =
+	 * new BridgeDomainAttachmentCircuitHelper();
+	 * 
+	 * }
+	 * 
+	 * @Test public void testAddPort() { ConnectivityServiceEndPoint cep = new
+	 * org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.connectivity.rev180307
+	 * .connectivity.service.EndPointBuilder()
+	 * .setServiceInterfacePoint(TapiUtils.toSipRef(new Uuid(UUID1),
+	 * ServiceInterfacePoint.class))
+	 * .setDirection(PortDirection.BIDIRECTIONAL).build(); ep1 = new EndPoint(cep,
+	 * null); port = ServicePort.toServicePort(ep1, NETCONF_TOPOLODY_NAME);
+	 * port.setVlanId(301L); BridgeDomainAttachmentCircuitHelper
+	 * bdAttachmentCircuitHelper1 = bdAttachmentCircuitHelper.addPort(port, false);
+	 * Assert.assertNotNull(bdAttachmentCircuitHelper1);
+	 * 
+	 * }
+	 * 
+	 * @Test public void testBuild() { BdAttachmentCircuits bdAttachmentCircuits =
+	 * bdAttachmentCircuitHelper.build();
+	 * Assert.assertNotNull(bdAttachmentCircuits); }
+	 */
+
+}
