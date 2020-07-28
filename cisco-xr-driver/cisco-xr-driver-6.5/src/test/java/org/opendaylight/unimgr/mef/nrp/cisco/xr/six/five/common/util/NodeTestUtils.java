01acaa11266e319c5d5e0a7e65d34edd57f76c3a
santanude <santanu.de@xoriant.com>
2020-07-28 09:03:10 +0200

    Multiple version of Cisco Xr Driver
    Signed-off-by: santanude <santanu.de@xoriant.com>
    Change-Id: I5f59962880503a35bc4564a8fbb2058be451c6cc
    Signed-off-by: Om Awasthi <om.SAwasthi@xoriant.com>
    Signed-off-by: Arif Hussain <om.SAwasthi@xoriant.com>
    Signed-off-by: santanude <santanu.de@xoriant.com>

---
+++ cisco-xr-driver/cisco-xr-driver-6.5/src/test/java/org/opendaylight/unimgr/mef/nrp/cisco/xr/six/five/common/util/NodeTestUtils.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2016 Cisco Systems Inc and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.unimgr.mef.nrp.cisco.xr.six.five.common.util;
+
+public final class NodeTestUtils {
+
+	/*
+	 * private NodeTestUtils() { }
+	 * 
+	 * public static final String DEVICE_ID = "device";
+	 * 
+	 * 
+	 * @SuppressWarnings({"unchecked","checkstyle:illegalcatch"}) public static
+	 * DataBroker mockDataBroker(Optional<Node> nodeOptional) { DataBroker
+	 * dataBroker = mock(DataBroker.class); final ReadTransaction transaction =
+	 * mock(ReadTransaction.class); final FluentFuture<Optional<Node>>
+	 * transactionResult = mock(FluentFuture.class);
+	 * 
+	 * try { when(transactionResult.get()).thenReturn(nodeOptional); } catch
+	 * (Exception e) { fail(e.getMessage()); }
+	 * when(transaction.read(eq(LogicalDatastoreType.OPERATIONAL),
+	 * any(InstanceIdentifier.class))).thenReturn(transactionResult);
+	 * when(dataBroker.newReadOnlyTransaction()).thenReturn(transaction);
+	 * 
+	 * return dataBroker; }
+	 * 
+	 * public static Optional<Node> mockNode() { final Node node = mock(Node.class);
+	 * when(node.getNodeId()).thenReturn(new NodeId(DEVICE_ID));
+	 * 
+	 * return Optional.of(node); }
+	 * 
+	 * public static Optional<Node> mockNetconfNode(boolean withNetconfCapabilities)
+	 * { List<AvailableCapability> netconfCapabilityList = new ArrayList<>(); if
+	 * (withNetconfCapabilities) { netconfCapabilityList =
+	 * Arrays.asList(createAvailableCapability(NetconfConstants.CAPABILITY_IOX_L2VPN
+	 * ), createAvailableCapability(NetconfConstants.CAPABILITY_IOX_IFMGR),
+	 * createAvailableCapability(NetconfConstants.CAPABILITY_IOX_INFRA_POLICYMGR));
+	 * }
+	 * 
+	 * AvailableCapabilities availableCapabilities =
+	 * Mockito.mock(AvailableCapabilities.class);
+	 * when(availableCapabilities.getAvailableCapability()).thenReturn(
+	 * netconfCapabilityList);
+	 * 
+	 * Optional<Node> mockedNodeOptional = mockNode(); NetconfNode netconfNode =
+	 * mock(NetconfNode.class);
+	 * when(netconfNode.getAvailableCapabilities()).thenReturn(availableCapabilities
+	 * );
+	 * 
+	 * Node mockedNode = mockedNodeOptional.get();
+	 * when(mockedNode.augmentation(NetconfNode.class)).thenReturn(netconfNode);
+	 * 
+	 * return mockedNodeOptional; }
+	 * 
+	 * private static AvailableCapability createAvailableCapability(String name) {
+	 * AvailableCapabilityBuilder availableCapabilityBuilder = new
+	 * AvailableCapabilityBuilder(); availableCapabilityBuilder.setCapability(name);
+	 * return availableCapabilityBuilder.build(); }
+	 */
+}
---
+++ cisco-xr-driver/cisco-xr-driver-6.5/src/test/java/org/opendaylight/unimgr/mef/nrp/cisco/xr/six/five/l2vpn/helper/PseudowireHelperTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2016 Cisco Systems Inc and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.unimgr.mef.nrp.cisco.xr.six.five.l2vpn.helper;
+
+/*
+ * @author krzysztof.bijakowski@amartus.com
+ */
+public class PseudowireHelperTest {
+
+	/*
+	 * @Test public void testBuild() { //given final Uint32 pseudowireId =
+	 * Uint32.valueOf(PseudowireHelper.generatePseudowireId()); Ipv4AddressNoZone
+	 * neighborAddress = new Ipv4AddressNoZone("1.2.3.4"); //when Pseudowires actual
+	 * = new PseudowireHelper().addPseudowire(neighborAddress).build(); //then
+	 * assertNotNull(actual); List<Pseudowire> actualPseudowireList =
+	 * actual.getPseudowire(); assertNotNull(actualPseudowireList); assertEquals(1,
+	 * actualPseudowireList.size()); Pseudowire actualPseudowire =
+	 * actualPseudowireList.get(0);
+	 * 
+	 * assertNotNull(actualPseudowire.getNeighbor()); assertEquals(1,
+	 * actualPseudowire.getNeighbor().size()); assertEquals(pseudowireId,
+	 * actualPseudowire.getPseudowireId().getValue());
+	 * 
+	 * List<Neighbor> actualNeighborList = actualPseudowire.getNeighbor();
+	 * assertNotNull(actualNeighborList); assertEquals(1,
+	 * actualNeighborList.size());
+	 * 
+	 * Neighbor actualNeighbor = actualNeighborList.get(0);
+	 * assertNotNull(actualNeighbor); assertEquals(neighborAddress,
+	 * actualNeighbor.getNeighbor()); }
+	 */
+}
