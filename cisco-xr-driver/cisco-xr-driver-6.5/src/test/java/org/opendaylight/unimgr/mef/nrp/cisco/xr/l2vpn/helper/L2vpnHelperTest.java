8393725ddc6510b27fbe1a8fcf303ea8b8c71a66
santanude <santanu.de@xoriant.com>
2020-07-28 09:03:10 +0200

    Multiple version of Cisco Xr Driver
    Signed-off-by: santanude <santanu.de@xoriant.com>
    Change-Id: I5f59962880503a35bc4564a8fbb2058be451c6cc
    Signed-off-by: Om Awasthi <om.SAwasthi@xoriant.com>

---
+++ cisco-xr-driver/cisco-xr-driver-6.5/src/test/java/org/opendaylight/unimgr/mef/nrp/cisco/xr/l2vpn/helper/L2vpnHelperTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2016 Cisco Systems Inc and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.unimgr.mef.nrp.cisco.xr.l2vpn.helper;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+/*import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.l2vpn.cfg.rev151109.L2vpn;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.l2vpn.cfg.rev151109.l2vpn.Database;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.l2vpn.cfg.rev151109.l2vpn.database.XconnectGroups;*/
+
+
+/*
+ * @author krzysztof.bijakowski@amartus.com
+ */
+public class L2vpnHelperTest {
+
+	/*
+	 * @Test public void build() { //given XconnectGroups xconnectGroups =
+	 * Mockito.mock(XconnectGroups.class);
+	 * 
+	 * //when L2vpn actual = L2vpnHelper.build(xconnectGroups);
+	 * 
+	 * //then Database actualDatabase = actual.getDatabase();
+	 * assertNotNull(actualDatabase);
+	 * 
+	 * XconnectGroups actualXconnectGroups = actualDatabase.getXconnectGroups();
+	 * assertEquals(xconnectGroups, actualXconnectGroups); }
+	 */
+}
