58b9535261a3807768e63c181687a569059117c6
santanude <santanu.de@xoriant.com>
2021-09-16 19:00:00 +0530

    Developed drivers for cisco xr 6.3 and 6.6
    Signed-off-by: santanude <santanu.de@xoriant.com>
    Change-Id: I3dd96066eb133095eec3ca24d957fbf35496e2c3

---
+++ cisco-xr-driver-6.6/src/main/java/org/opendaylight/unimgr/mef/nrp/cisco/xr/six/five/common/ServicePort.java
@@ -0,0 +1,155 @@
+/*
+ * Copyright (c) 2016 Cisco Systems Inc and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.unimgr.mef.nrp.cisco.xr.six.five.common;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import org.opendaylight.unimgr.mef.nrp.api.EndPoint;
+import org.opendaylight.unimgr.mef.nrp.cisco.xr.six.five.common.util.SipHandler;
+import org.opendaylight.yang.gen.v1.urn.mef.yang.nrm.connectivity.rev180321.carrier.eth.connectivity.end.point.resource.EgressBwpFlow;
+import org.opendaylight.yang.gen.v1.urn.mef.yang.nrm.connectivity.rev180321.carrier.eth.connectivity.end.point.resource.IngressBwpFlow;
+import org.opendaylight.yang.gen.v1.urn.mef.yang.nrp._interface.rev180321.nrp.connectivity.service.end.point.attrs.NrpCarrierEthConnectivityEndPointResource;
+import org.opendaylight.yang.gen.v1.urn.onf.otcc.yang.tapi.common.rev180307.Uuid;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TpId;
+
+
+
+/**
+ * Class representing port (replacement for FcPort).
+ *
+ * @author marek.ryznar@amartus.com
+ */
+public class ServicePort {
+    private static final String PATTERN = ".+?(?=((((\\d+)/)+)\\d+))";
+    private static final Pattern INTERFACE_NAME_PATTERN = Pattern.compile(PATTERN);
+    private static final String DEFAULT_INTERFACE_NAME = "GigabitEthernet";
+
+    private IngressBwpFlow ingressBwpFlow;
+    private EgressBwpFlow egressBwpFlow;
+
+    //netconf topology
+    private final TopologyId topoId;
+    //represents device ie dev-68 in netconf topology
+    private final  NodeId nodeId;
+    //defines port
+    private final TpId tpId;
+    //defines cTag VLAN ID
+    private Long vlanId = null;
+
+    public ServicePort(TopologyId topoId, NodeId nodeId, TpId tpId) {
+        this.topoId = topoId;
+        this.nodeId = nodeId;
+        this.tpId = tpId;
+    }
+
+    public TopologyId getTopology() {
+        return topoId;
+    }
+
+
+    public NodeId getNode() {
+        return nodeId;
+    }
+
+    public TpId getTp() {
+        return tpId;
+    }
+
+
+    public Long getVlanId() {
+        return vlanId;
+    }
+
+    public void setVlanId(Long vlanId) {
+        this.vlanId = vlanId;
+    }
+
+    public static ServicePort toServicePort(EndPoint endPoint, String topologyName) {
+        return toServicePort(endPoint, new TopologyId(topologyName));
+    }
+
+    public static ServicePort toServicePort(EndPoint endPoint, TopologyId topologyId) {
+        Uuid sip = endPoint.getEndpoint().getServiceInterfacePoint().getServiceInterfacePointId();
+        NodeId nodeId = new NodeId(SipHandler.getDeviceName(sip));
+        TpId tpId = new TpId(SipHandler.getPortName(sip));
+        ServicePort servicePort = new ServicePort(topologyId,nodeId,tpId);
+        if (hasVlan(endPoint)) {
+            servicePort.setVlanId((long) getVlan(endPoint));
+        }
+        return servicePort;
+    }
+
+    public static boolean hasVlan(EndPoint endPoint) {
+        if ((endPoint.getAttrs() != null)
+                && (endPoint.getAttrs().getNrpCarrierEthConnectivityEndPointResource() != null)) {
+            NrpCarrierEthConnectivityEndPointResource attr =
+                    endPoint.getAttrs().getNrpCarrierEthConnectivityEndPointResource();
+            if ((attr.getCeVlanIdListAndUntag() != null)
+                    && !(attr.getCeVlanIdListAndUntag().getVlanId().isEmpty())) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    private static int getVlan(EndPoint endPoint) {
+        return endPoint.getAttrs().getNrpCarrierEthConnectivityEndPointResource()
+                .getCeVlanIdListAndUntag().getVlanId().get(0).getVlanId().getValue().intValue();
+    }
+
+    public static boolean isSameDevice(EndPoint endPoint, List<String> ls) {
+        Uuid sip = endPoint.getEndpoint().getServiceInterfacePoint().getServiceInterfacePointId();
+        NodeId nodeId = new NodeId(SipHandler.getDeviceName(sip));
+
+        if (ls.size() == 0) {
+            ls.add(nodeId.getValue());
+        } else if (ls.size() > 0) {
+            List<String> listWithoutDuplicates =
+                    ls.stream().distinct().collect(Collectors.toList());
+
+            java.util.Optional<String> preset = listWithoutDuplicates.stream()
+                    .filter(x -> x.equals(nodeId.getValue())).findFirst();
+
+            if (preset.isPresent()) {
+                return true;
+            }
+            ls.add(nodeId.getValue());
+        }
+
+        return false;
+    }
+
+    public String getInterfaceName() {
+       // TpId tpId = this.getTp();
+        Matcher matcher = INTERFACE_NAME_PATTERN.matcher(this.getTp().getValue());
+        return matcher.find() ? matcher.group() : DEFAULT_INTERFACE_NAME;
+    }
+
+    public IngressBwpFlow getIngressBwpFlow() {
+        return ingressBwpFlow;
+    }
+
+    public void setIngressBwpFlow(IngressBwpFlow ingressBwpFlow) {
+        this.ingressBwpFlow = ingressBwpFlow;
+    }
+
+    public EgressBwpFlow getEgressBwpFlow() {
+        return egressBwpFlow;
+    }
+
+    public void setEgressBwpFlow(EgressBwpFlow egressBwpFlow) {
+        this.egressBwpFlow = egressBwpFlow;
+    }
+}
---
+++ cisco-xr-driver-6.6/src/main/java/org/opendaylight/unimgr/mef/nrp/cisco/xr/six/five/common/helper/InterfaceHelper.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2016 Cisco Systems Inc and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.unimgr.mef.nrp.cisco.xr.six.five.common.helper;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+import org.opendaylight.unimgr.mef.nrp.cisco.xr.six.five.common.ServicePort;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev170907.InterfaceActive;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev170907.InterfaceConfigurations;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev170907.InterfaceConfigurationsBuilder;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev170907.InterfaceModeEnum;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev170907._interface.configurations.InterfaceConfiguration;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev170907._interface.configurations.InterfaceConfigurationBuilder;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.ifmgr.cfg.rev170907._interface.configurations._interface.configuration.Mtus;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.l2.eth.infra.cfg.rev180615.InterfaceConfiguration2;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.l2.eth.infra.cfg.rev180615.InterfaceConfiguration2Builder;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.l2.eth.infra.cfg.rev180615._interface.configurations._interface.configuration.EthernetServiceBuilder;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.l2.eth.infra.cfg.rev180615._interface.configurations._interface.configuration.ethernet.service.Encapsulation;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.l2.eth.infra.cfg.rev180615._interface.configurations._interface.configuration.ethernet.service.EncapsulationBuilder;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.l2.eth.infra.datatypes.rev151109.Match;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.l2.eth.infra.datatypes.rev151109.VlanTagOrAny;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.l2vpn.cfg.rev180615.InterfaceConfiguration4;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.l2vpn.cfg.rev180615.InterfaceConfiguration4Builder;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.l2vpn.cfg.rev180615._interface.configurations._interface.configuration.L2Transport;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.ios.xr.l2vpn.cfg.rev180615._interface.configurations._interface.configuration.L2TransportBuilder;
+import org.opendaylight.yang.gen.v1.http.cisco.com.ns.yang.cisco.xr.types.rev180629.InterfaceName;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.opendaylight.yangtools.yang.common.Empty;
+import org.opendaylight.yangtools.yang.common.Uint32;
+
+/*
+ * Helper, designated to support interface configuration
+ *
+ * @author krzysztof.bijakowski@amartus.com
+ */
+public class InterfaceHelper {
+    private List<InterfaceConfiguration> configurations;
+
+    public static InterfaceName getInterfaceName(ServicePort port) {
+        String interfaceName = port.getTp().getValue();
+
+        if (interfaceName.contains(":")) {
+            interfaceName = interfaceName.split(":")[1];
+        }
+
+        return new InterfaceName(interfaceName);
+    }
+
+    public static InterfaceName getSubInterfaceName(ServicePort port) {
+        String interfaceName = port.getTp().getValue();
+
+        if (interfaceName.contains(":")) {
+            interfaceName = interfaceName.split(":")[1];
+        }
+        // adding vlan id with interface name
+        interfaceName = interfaceName + "." + port.getVlanId();
+        return new InterfaceName(interfaceName);
+    }
+
+
+    public static InstanceIdentifier<InterfaceConfigurations> getInterfaceConfigurationsId() {
+        return InstanceIdentifier.builder(InterfaceConfigurations.class).build();
+    }
+
+    public InterfaceHelper() {
+        configurations = new LinkedList<>();
+    }
+
+    public InterfaceHelper addInterface(ServicePort port, Optional<Mtus> mtus, boolean setL2Transport) {
+        return addInterface(getInterfaceName(port), mtus, setL2Transport);
+    }
+
+    public InterfaceHelper addInterface(InterfaceName name, Optional<Mtus> mtus, boolean setL2Transport) {
+        InterfaceConfigurationBuilder configurationBuilder = new InterfaceConfigurationBuilder();
+
+        configurationBuilder
+            .setInterfaceName(name)
+            .setActive(new InterfaceActive("act"));
+
+        if (mtus.isPresent()) {
+            configurationBuilder.setMtus(mtus.get());
+        }
+
+        if (setL2Transport) {
+            setL2Configuration(configurationBuilder);
+        }
+
+        configurations.add(configurationBuilder.build());
+        return this;
+    }
+
+    public InterfaceHelper addSubInterface(ServicePort port, Optional<Mtus> mtus) {
+        return addSubInterface(getSubInterfaceName(port), mtus, port);
+    }
+
+    public InterfaceHelper addSubInterface(InterfaceName name, Optional<Mtus> mtus, ServicePort port) {
+        InterfaceConfigurationBuilder configurationBuilder = new InterfaceConfigurationBuilder();
+
+        configurationBuilder
+            .setInterfaceName(name)
+            .setActive(new InterfaceActive("act"))
+            //.setShutdown(Boolean.FALSE)
+            .setDescription("Create sub interface through ODL")
+            .setInterfaceModeNonPhysical(InterfaceModeEnum.L2Transport);
+            // set ethernet service
+        setEthernetService(configurationBuilder, port);
+
+        if (mtus.isPresent()) {
+            configurationBuilder.setMtus(mtus.get());
+        }
+        configurations.add(configurationBuilder.build());
+
+        return this;
+    }
+
+    private void setEthernetService(InterfaceConfigurationBuilder configurationBuilder, ServicePort port) {
+        Encapsulation encapsulation = new EncapsulationBuilder()
+            .setOuterRange1Low(new VlanTagOrAny(Uint32.valueOf(port.getVlanId())))
+            .setOuterTagType(Match.MatchDot1q)
+            .build();
+
+        InterfaceConfiguration2 augmentation = new InterfaceConfiguration2Builder()
+                //.setEthernetService(new EthernetServiceBuilder()
+                //    .setEncapsulation(encapsulation)
+                 //   .build()
+               // )
+                .build();
+
+        configurationBuilder.addAugmentation(InterfaceConfiguration2.class, augmentation);
+    }
+
+    public InterfaceConfigurations build() {
+        return new InterfaceConfigurationsBuilder()
+            .setInterfaceConfiguration(configurations)
+            .build();
+    }
+
+    private void setL2Configuration(InterfaceConfigurationBuilder configurationBuilder) {
+        L2Transport l2transport = new L2TransportBuilder()
+                    .setEnabled(Empty.getInstance())
+                    .build();
+
+        InterfaceConfiguration4 augmentation = new InterfaceConfiguration4Builder()
+            .setL2Transport(l2transport)
+            .build();
+
+        configurationBuilder.addAugmentation(InterfaceConfiguration4.class, augmentation);
+    }
+
+}
